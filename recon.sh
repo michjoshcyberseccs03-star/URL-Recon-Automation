58efa8cea33f01dedd9e1768a3486f458854d4de
michjoshcyberseccs03-star <michjoshcybersec.cs03@gmail.com>
2026-06-24 15:11:14 +0530

    Create recon.sh

---
+++ recon.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+[ -z "$1" ] && echo "Usage: $0 example.com" && exit 1
+
+d=$1
+
+out="recon_$d"
+
+mkdir -p "$out/input"
+mkdir -p "$out/gf"
+
+echo "[+] Running gau..."
+echo "$d" | gau > "$out/input/gau.txt"
+
+echo "[+] Running waybackurls..."
+echo "$d" | waybackurls > "$out/input/wayback.txt"
+
+echo "[+] Running katana..."
+katana -u "https://$d" -silent -depth 2 > "$out/input/katana.txt"
+
+echo "[+] Merging URLs..."
+cat \
+"$out/input/gau.txt" \
+"$out/input/wayback.txt" \
+"$out/input/katana.txt" \
+| sort -u > "$out/input/all.txt"
+
+echo "[+] Running httpx..."
+httpx -l "$out/input/all.txt" \
+-silent \
+-fc 404,400 \
+-random-agent \
+> "$out/input/live.txt"
+
+echo "[+] Running gf patterns..."
+
+gf sqli < "$out/input/live.txt" > "$out/gf/sqli.txt"
+gf xss < "$out/input/live.txt" > "$out/gf/xss.txt"
+gf lfi < "$out/input/live.txt" > "$out/gf/lfi.txt"
+gf redirect < "$out/input/live.txt" > "$out/gf/redirect.txt"
+gf ssrf < "$out/input/live.txt" > "$out/gf/ssrf.txt"
+gf rce < "$out/input/live.txt" > "$out/gf/rce.txt"
+gf idor < "$out/input/live.txt" > "$out/gf/idor.txt"
+gf ssti < "$out/input/live.txt" > "$out/gf/ssti.txt"
+
+
+echo
+echo "[+] DONE"
+echo "[+] Output Folder : $out"
